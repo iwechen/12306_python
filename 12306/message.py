abb8c696e5025858e454cb4eb37eaf0f199cb7aa
iwechen <iwechen123@gmail.com>
2018-01-15 14:28:22 +0800

    创建12306购票系统

---
+++ 12306/message.py
@@ -0,0 +1,16 @@
+#12306 登录用户名
+userName = '1639663168@qq.com'
+#12306 登录密码
+password = 'cw913288'
+#12306 验证码本地地址
+captchaDownloadName = 'captcha.png'
+#12306 站名代码存放地址
+stationDownloadName = 'stationCode.txt'
+#打码兔api 用户名
+damaUserName = ''
+#打码兔api 密码
+damaPassword = ''
+#打码兔api key
+damaKey = ''
+#打码兔id
+damaId = ''
